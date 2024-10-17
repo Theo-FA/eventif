bd886519b84095bf85f763c520ae86162c2e5e06
Théo Ferraz de Almeida <101749741+Theo-FA@users.noreply.github.com>
2024-10-17 01:33:09 -0300

    Update settings.py

--- eventif/settings.py
+++ eventif/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "eventif.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
