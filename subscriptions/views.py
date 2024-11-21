aaa57783cc364cd4c60dd5d3fcf7497fb6c279cc
Théo Ferraz de Almeida <101749741+Theo-FA@users.noreply.github.com>
2024-11-21 19:12:39 -0300

    Update views.py

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,12 +1,11 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from subscriptions.forms import SubscriptionForm
-from subscriptions.models import Subscription
 from django.core import mail
 from django.template.loader import render_to_string
 from django.contrib import messages
 from django.conf import settings
-
+from subscriptions.models import Subscription
 
 def subscribe(request):
     if request.method == 'POST':
@@ -14,6 +13,13 @@ def subscribe(request):
     else:
         return new(request)
 
+def detail(request, pk):
+    try:
+        subscription = Subscription.objects.get(pk=pk)
+    except Subscription.DoesNotExist:
+        raise Http404
+
+    return render(request, 'subscriptions/subscription_detail.html', {'subscription': subscription})
 
 def create(request):
     form = SubscriptionForm(request.POST)
@@ -21,22 +27,19 @@ def create(request):
     if not form.is_valid():
         return render(request, 'subscriptions/subscription_form.html', {'form': form})
 
+    subscription = Subscription.objects.create(**form.cleaned_data)
+
     _send_mail(
         'subscriptions/subscription_email.txt',
-        form.cleaned_data,
+        {'subscription': subscription},
         'Confirmação de inscrição!',
         settings.DEFAULT_FROM_EMAIL,
-        form.cleaned_data['email'])
+        subscription.email)
 
-    Subscription.objects.create(**form.cleaned_data)
-
-    messages.success(request, 'Inscrição realizada com sucesso!')
-    return HttpResponseRedirect('/inscricao/')
-
+    return HttpResponseRedirect('/inscricao/{}/'.format(subscription.pk))
 
 def new(request):
     return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
-
 
 def _send_mail(template_name, context, subject, from_, to):
     body = render_to_string(template_name, context)
